7a89e8f68eb38970c6b4accc9c7196e8044a5114
habin2243 <habin2243@risingwings.com>
2025-07-29 08:07:14 +0900

    Update server files - 2025-07-29  8:07:14.34

---
+++ bat_home/quick_push_client.bat
@@ -0,0 +1,17 @@
+@echo off
+echo HoloDuel Client - Quick Push
+echo ============================
+
+REM 클라이언트 디렉토리로 이동
+cd /d "D:\OneDrive\OneDrive - KRAFTON\cursor_study\holoduel\holocardclient-main"
+
+REM 모든 변경사항 추가 및 커밋
+git add .
+git commit -m "Update client files - %date% %time%"
+
+REM GitHub에 push (클라이언트 저장소)
+git push origin master
+
+echo.
+echo Client Push 완료!
+pause 
